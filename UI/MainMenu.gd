2b02f968619a2f7402635b24bbf391f71ce22c08
Anden Acitelli <a.acitelli@gmail.com>
2020-07-06 14:24:45 -0400

    Add escape=gamequit functionality to menu system

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -3,6 +3,11 @@ extends MarginContainer
 func _ready():
 	pass
 
+func _process(_delta):
+	# Handle keyboard input not specific to the player
+	if Input.is_action_pressed("exit"):
+		get_tree().quit()
+
 func _on_PlayGameButton_pressed():
 	print("Button Pressed! Changing scene.")
 	get_tree().change_scene("res://Core/Main.tscn")
