a63fdd4c3ebd3c045012579f0b2bfc8a9a029a2f
Anden Acitelli <a.acitelli@gmail.com>
2020-07-06 15:28:03 -0400

    Implemented basic options menuing system

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -1,13 +1,65 @@
-extends MarginContainer
+extends Control
+
+var main_menu_bp
+var options_menu_bp
 
 func _ready():
-	pass
+	main_menu_bp = "MainMenuBackground/MainMenu"
+	options_menu_bp = "MainMenuBackground/OptionsMenuContainer/OptionsMenu/"
 
 func _process(_delta):
-	# Handle keyboard input not specific to the player
 	if Input.is_action_pressed("exit"):
 		get_tree().quit()
 
 func _on_PlayGameButton_pressed():
-	print("Button Pressed! Changing scene.")
 	get_tree().change_scene("res://Core/Main.tscn")
+
+func _on_OptionsButton_pressed():
+
+	# Hide Main Menu
+	get_node(main_menu_bp).hide()
+
+	# Make everything but the options section we want invisible
+	get_node(options_menu_bp + "Audio").hide()
+	get_node(options_menu_bp + "Video").hide()
+	get_node(options_menu_bp + "Graphics").hide()
+	get_node(options_menu_bp + "Controls").hide()
+	get_node(options_menu_bp + "Gameplay").show()
+
+	# Make the overall options menu visible
+	get_node("MainMenuBackground/OptionsMenuContainer").show()
+
+func _on_GameplaySectionButton_pressed():
+	get_node(options_menu_bp + "Audio").hide()
+	get_node(options_menu_bp + "Video").hide()
+	get_node(options_menu_bp + "Graphics").hide()
+	get_node(options_menu_bp + "Controls").hide()
+	get_node(options_menu_bp + "Gameplay").show()
+
+func _on_AudioSectionButton_pressed():
+	get_node(options_menu_bp + "Video").hide()
+	get_node(options_menu_bp + "Graphics").hide()
+	get_node(options_menu_bp + "Controls").hide()
+	get_node(options_menu_bp + "Gameplay").hide()
+	get_node(options_menu_bp + "Audio").show()
+
+func _on_VideoSectionButton_pressed():
+	get_node(options_menu_bp + "Audio").hide()
+	get_node(options_menu_bp + "Graphics").hide()
+	get_node(options_menu_bp + "Controls").hide()
+	get_node(options_menu_bp + "Gameplay").hide()
+	get_node(options_menu_bp + "Video").show()
+
+func _on_GraphicsSectionButton_pressed():
+	get_node(options_menu_bp + "Audio").hide()
+	get_node(options_menu_bp + "Video").hide()
+	get_node(options_menu_bp + "Controls").hide()
+	get_node(options_menu_bp + "Gameplay").hide()
+	get_node(options_menu_bp + "Graphics").show()
+
+func _on_ControlsSectionButton_pressed():
+	get_node(options_menu_bp + "Audio").hide()
+	get_node(options_menu_bp + "Video").hide()
+	get_node(options_menu_bp + "Graphics").hide()
+	get_node(options_menu_bp + "Gameplay").hide()
+	get_node(options_menu_bp + "Controls").show()
